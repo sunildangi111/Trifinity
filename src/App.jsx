8a04514506af47eaf58996021c1c493e3ad88169
sunildangi111 <149437045+sunildangi111@users.noreply.github.com>
2026-05-05 03:32:31 +0530

    fix router and deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TutorForm from "./pages/TutorForm";
 import StudentForm from "./pages/StudentForm";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tutor" element={<TutorForm />} />
-        <Route path="/student" element={<StudentForm />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tutor" element={<TutorForm />} />
+      <Route path="/student" element={<StudentForm />} />
+    </Routes>
   );
 }
 
-export default App;
+export default App;   // ✅ VERY IMPORTANT
